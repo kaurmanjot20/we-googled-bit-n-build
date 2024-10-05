317855bcd2df6628ec1ffc92a660cd08014da0f7
Manjot Kaur <kaurmanjot2074@outlook.com>
2024-10-05 19:11:16 +0530

    frontend user ui basic setup

---
+++ frontend-user/src/components/NeverHaveIEver.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NeverHaveIEver() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NeverHaveIEver
---
+++ frontend-user/src/components/WhosMoreLikely.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WhosMoreLikely() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WhosMoreLikely
